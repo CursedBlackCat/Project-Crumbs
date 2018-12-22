e852001a9ba774748637faee6f027886f46d4caa
Darren Yip <darrenyip@live.com>
2018-12-22 17:31:34 -0500

    Sticker sending working

--- projectcrumbs/src/main/java/me/cursedblackcat/projectcrumbs/StickerPack.java
+++ projectcrumbs/src/main/java/me/cursedblackcat/projectcrumbs/StickerPack.java
@@ -1,5 +1,6 @@
 package me.cursedblackcat.projectcrumbs;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import javafx.scene.image.Image;
@@ -7,11 +8,13 @@ import javafx.scene.image.Image;
 public class StickerPack {
 	String packName;
 	ArrayList<Image>  stickers;
+	ArrayList<File> stickersFile;
 	Image icon;
 	
-	public StickerPack(String name, ArrayList<Image>  stickers, Image icon) {
+	public StickerPack(String name, ArrayList<Image> stickers, ArrayList<File> stickersF, Image icon) {
 		packName = name;
 		this.stickers = stickers;
+		stickersFile = stickersF;
 		this.icon = icon;
 	}
 	
@@ -23,7 +26,15 @@ public class StickerPack {
 		return stickers;
 	}
 	
+	public ArrayList<File> getStickersFile() {
+		return stickersFile;
+	}
+	
 	public Image getIcon() {
 		return icon;
+	}
+	
+	public File getFile(Image i) {
+		return stickersFile.get(stickers.indexOf(i));
 	}
 }
